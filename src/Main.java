012346c2fb8316699cf14e4b04cf9b65c8ef79bd
nguyenquangos0302work <nguyenquangos0302work@gmail.com>
2024-12-17 20:30:36 +0700

    auto generate sql file

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,54 @@
-/*
- * Classname
- *
- * Version information
- *
- * Date
- *
- * Copyright notice
- *
- * Modification Logs:
- *      DATE            AUTHOR              DESCRIPTION
- *  -----------------------------------------------------------------------------------------
- *  10-Aug-2003         QuangNTN            Description of modification
- */
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        String title = "177. Nth Highest Salary";
-        System.out.println(modifyTitleIntoClassName(title));
+        String typeSql = ".sql";
+        List<String> titles = getTitles();
+        for (String title : titles) {
+            if (typeSql.equals(".sql")) {
+                String fileName = modifyTitleIntoClassName(title, typeSql);
+                generateSqlFile(fileName);
+            } else {
+                System.out.println(modifyTitleIntoClassName(title, typeSql));
+            }
+        }
     }
 
-    public static String modifyTitleIntoClassName(String title) {
+    private static List<String> getTitles() {
+        return List.of("1378. Replace Employee ID With The Unique Identifier",
+                "1068. Product Sales Analysis I",
+                "1581. Customer Who Visited but Did Not Make Any Transactions",
+                "197. Rising Temperature",
+                "1661. Average Time of Process per Machine",
+                "577. Employee Bonus",
+                "1280. Students and Examinations",
+                "570. Managers with at Least 5 Direct Reports",
+                "1934. Confirmation Rate");
+    }
+
+    private static String modifyTitleIntoClassName(String title, String type) {
         if (title == null || title.isEmpty()) return "";
-        return "_" + title.replaceAll("\\s+|\\.\\s", "_");
+        if (type.equals(".sql")) return title.replaceAll("\\s+|\\.\\s", "_") + type;
+        return "_" + title.replaceAll("\\s+|\\.\\s", "_") + type;
+    }
+
+    private static void generateSqlFile(String fileName) {
+        File file = new File(fileName);
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            writer.write("DROP SCHEMA IF EXISTS leetcode CASCADE;\n");
+            writer.write("CREATE SCHEMA leetcode;\n");
+            writer.write("SET search_path TO leetcode, public;\n");
+
+            writer.flush();
+            System.out.println("File created successfully: " + fileName);
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing the file: " + fileName);
+            e.printStackTrace();
+        }
     }
 
 }
